ccb09d8c71bae890fbfe06071feca4c27b30ab38
anicdao <amielnicdao@gmail.com>
2019-10-05 22:28:46 -0400

    added variables to display wins, losses, and blank spaces for user input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,18 +9,37 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 15;
 var guesses = [];
+var computerGuess = 0;
+var userGuess = 0;
 
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guessesleft-text");
 var lettersGuessedText = document.getElementById("lettersguessed-text");
+var computerGuess = words[Math.floor(Math.random() * words.length)];
 
 function startGame() {
-    computerGuess = letters[Math.floor(Math.random() * letters.length)];
+     computerGuess = words[Math.floor(Math.random() * words.length)];
+  }
+
+  for (var i = 0; i < computerGuess.length; i++) {
+      guesses.push("_");
   }
   
   document.onkeyup = function (event) {
     
-      var userGuess = event.key;
-
+    userGuess = event.key;    
   }
+
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+lettersGuessedText.textContent = "Your guesses: ";
+var newguesses = guesses.join(" ");
+
+guesses.push(userGuess);
+document.getElementById("allguesses").innerHTML = newguesses;
+
+
+startGame()
+
