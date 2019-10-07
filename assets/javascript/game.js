edb3b6f614cc26cc3160764b6915567db32577f8
anicdao <amielnicdao@gmail.com>
2019-10-06 20:40:19 -0400

    added a for loop and a variable to store the blanks and correct guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,42 +5,63 @@
 //need to be able to restart game after a win or a loss
 
 var words = ["modern warfare", "activision", "search and destroy", "infinity ward", "deathmatch"];
+
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 var wins = 0;
 var losses = 0;
 var guessesLeft = 15;
 var guesses = [];
-var winningWord = [];
-var userGuess = 0;
+var rightGuesses = [];
+var guessedLetter = 0; //added
 
+var rightGuessesText = document.getElementById("rightguesses-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guessesleft-text");
 var lettersGuessedText = document.getElementById("lettersguessed-text");
-var computerGuess = words[Math.floor(Math.random() * words.length)];
+var computerGuess;
 
 function startGame() {
-     computerGuess = words[Math.floor(Math.random() * words.length)];
-  }
+  computerGuess = words[Math.floor(Math.random() * words.length)];
+  console.log(computerGuess);
 
-  for (var i = 0; i < computerGuess.length; i++) {
-      guesses.push("_");
+  for (var i = 0; i < guessedLetter; i++) {
+    guesses.push("_");
   }
+}
+
+
+
+document.onkeyup = function (event) {
+
+  var userGuess = event.key;
+
   
-  document.onkeyup = function (event) {
-    
-    userGuess = event.key;    
-  }
 
-winsText.textContent = "Wins: " + wins;
-lossesText.textContent = "Losses: " + losses;
-guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
-lettersGuessedText.textContent = "Your guesses: ";
-var newguesses = guesses.join(" ");
 
-guesses.push(userGuess);
-document.getElementById("allguesses").innerHTML = newguesses;
+  // if (userGuess === computerGuess) {
+  //   wins++;
+  //   guessesLeft = 15;
+  //   guesses = [];
+  //   startGame()
+  // } if (userGuess !== computerGuess) {
+  //   guessesLeft--;
+  // } if (guessesLeft === 0) {
+  //   losses++;
+  //   guessesLeft = 15;
+  //   startGame()
+  //   guesses = [];
+  // }
 
+  winsText.textContent = "Wins: " + wins;
+  lossesText.textContent = "Losses: " + losses;
+  guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+  lettersGuessedText.textContent = "Your guesses: ";
+  var newguesses = guesses.join(", ");
+
+  guesses.push(userGuess);
+  document.getElementById("allguesses").innerHTML = newguesses;
+};
 
 startGame()
-
